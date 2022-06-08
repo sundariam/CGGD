6a0a19631fe754e3982a3b183e7c011626636790
sundariam <girldarina@mail.ru>
2022-06-09 00:19:36 +0300

    Implement image clearing & saving in cg::renderer::rasterization_renderer class

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -5,7 +5,12 @@
 
 void cg::renderer::rasterization_renderer::init()
 {
-	// TODO: Lab 1.02. Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
+			settings->width, settings->height
+			);
+	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
+	rasterizer->set_viewport(settings->width, settings->height);
+	rasterizer->set_render_target(render_target);
 	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
@@ -13,7 +18,9 @@ void cg::renderer::rasterization_renderer::init()
 }
 void cg::renderer::rasterization_renderer::render()
 {
-	// TODO: Lab 1.02. Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
+	rasterizer->clear_render_target({111,15,12});
+
+	cg::utils::save_resource(*render_target, settings->result_path);
 	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	// TODO: Lab 1.04. Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
