dd1f4d481d8199f21bfbb9ba22b6dabb0a6b08b9
sundariam <girldarina@mail.ru>
2022-07-07 17:15:38 +0300

    Implement `wait_for_gpu` method

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -488,6 +488,17 @@ void cg::renderer::dx12_renderer::load_assets()
 
 	create_constant_buffer_view(constant_buffer,
 								cbv_srv_heap.get_cpu_descriptor_handle(0));
+
+	//Create a fence
+	THROW_IF_FAILED(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+	fence_event = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	if (fence_event == nullptr)
+	{
+		THROW_IF_FAILED(HRESULT_FROM_WIN32(GetLastError()));
+	}
+
+	//wait_for_gpu();
+
 }
 
 
@@ -567,7 +578,17 @@ void cg::renderer::dx12_renderer::move_to_next_frame()
 
 void cg::renderer::dx12_renderer::wait_for_gpu()
 {
-	// TODO Lab 3.07. Implement `wait_for_gpu` method
+	THROW_IF_FAILED(command_queue->Signal(
+			fence.Get(),
+			fence_values[frame_index]
+			));
+	THROW_IF_FAILED(fence->SetEventOnCompletion(
+			fence_values[frame_index],
+			fence_event
+			));
+	WaitForSingleObjectEx(fence_event, INFINITE, FALSE);
+	fence_values[frame_index]++;
+
 }
 
 void cg::renderer::descriptor_heap::create_heap(ComPtr<ID3D12Device>& device, D3D12_DESCRIPTOR_HEAP_TYPE type, UINT number, D3D12_DESCRIPTOR_HEAP_FLAGS flags)
